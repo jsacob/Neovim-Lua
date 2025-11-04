d8fd068ca78d304ac35e80b7d26f26ac95f6368e
jacob <jacobmofficial@outlook.com>
2025-11-04 14:06:34 -0500

    removed_lua_l

--- .config/nvim/lua/lsp/lualsp.lua
+++
@@ -1,9 +0,0 @@
-vim.lsp.enable('luals',({
-	settings = {
-		Lua = {
-			runtime = { version = 'LuaJIT' },
-			diagnostics = { globals = { 'vim' } },
-			workspace = { library = { vim.env.VIMRUNTIME } }
-		}
-	},
-}))
