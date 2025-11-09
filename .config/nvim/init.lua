f11cdad5f25caca77bf3cce27eeaf1eb63bee899
jacob <jacobmofficial@outlook.com>
2025-11-09 22:55:37 +0000

    up

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,3 +41,8 @@ require("config.telescope")
 
 require("config.toggleterm");
 
+-- local opts = { noremap = true, silent = true }
+-- vim.keymap.set('n', 'j', 'jzz', opts)
+-- vim.keymap.set('n', 'k', 'kzz', opts)
+-- vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+-- vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
